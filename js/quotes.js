be479af833f2355aeaa3c9919747f7455a248cb6
shahadot99999 <shahadot99999@gmail.com>
2022-08-29 15:46:28 +0600

    first commit

---
+++ js/quotes.js
@@ -0,0 +1,12 @@
+const loadQuote = () => {
+    fetch('https://api.kanye.rest/')
+        .then(res => res.json())
+        .then(data => displayQuote(data))
+}
+
+const displayQuote = quote => {
+    const blockQuote = document.getElementById('quote');
+    console.log(quote);
+    //object under object 
+    blockQuote.innerText = quote.quote;
+}
